545238a2213f8710707738b6096043e9ac1e0a7b
Jonas Sjömark <jonas.sjomark@kungsbacka.se>
2024-11-19 15:30:12 +0100

    Simplify the enum converter

--- src/Converters/UpperCaseEnumConverter.cs
+++ src/Converters/UpperCaseEnumConverter.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace FrejaOrgId.Converters
@@ -23,63 +22,24 @@ namespace FrejaOrgId.Converters
         /// </exception>
         public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            if (typeToConvert.IsArray)
+            if (Enum.TryParse(typeToConvert, reader.GetString(), ignoreCase: true, out object? enumValue))
             {
-                Type? elementType = typeToConvert.GetElementType() ??
-                                    throw new JsonException("Unable to determine array element type");
-                ArrayList list = [];
-                while (reader.Read() && reader.TokenType != JsonTokenType.EndArray)
-                {
-                    if (Enum.TryParse(elementType, reader.GetString(), ignoreCase: true, out object? enumValue))
-                    {
-                        list.Add(enumValue);
-                    }
-                    else
-                    {
-                        throw new JsonException("Invalid enum value in array");
-                    }
-                }
-
-                Array array = Array.CreateInstance(typeToConvert, list.Count);
-                list.CopyTo(array);
-                return (T)Convert.ChangeType(array, typeof(T));
+                return (T)enumValue;
             }
             else
             {
-                if (Enum.TryParse(typeToConvert, reader.GetString(), ignoreCase: true, out object? enumValue))
-                {
-                    return (T)enumValue;
-                }
-                else
-                {
-                    throw new JsonException("Invalid enum value");
-                }
+                throw new JsonException("Invalid enum value");
             }
         }
 
         /// <summary>
-        /// Writes the specified value as a JSON string using the provided Utf8JsonWriter. If the value is an array,
-        /// each element is written as an uppercase string in a JSON array. If the value is not an array, it is written
-        /// as an uppercase string. Throws a JsonException if any enum value is null.
+        /// Writes the specified value as a JSON string using the provided Utf8JsonWriter. This will also handle
+        /// the case where the value is an array of enums. Throws a JsonException if any enum value is null.
         /// </summary>
         public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
         {
-            if (value is Array enumArray)
-            {
-                writer.WriteStartArray();
-                foreach (object enumValue in enumArray)
-                {
-                    string stringValue = enumValue?.ToString() ?? throw new JsonException("Enum value cannot be null");
-                    writer.WriteStringValue(stringValue.ToUpperInvariant());
-                }
-
-                writer.WriteEndArray();
-            }
-            else
-            {
-                string stringValue = value?.ToString() ?? throw new JsonException("Enum value cannot be null");
-                writer.WriteStringValue(stringValue.ToUpperInvariant());
-            }
+            string stringValue = value?.ToString() ?? throw new JsonException("Enum value cannot be null");
+            writer.WriteStringValue(stringValue.ToUpperInvariant());
         }
     }
 }
